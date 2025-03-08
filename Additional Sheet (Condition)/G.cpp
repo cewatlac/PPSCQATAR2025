fcf56cace48adb170c2780a64400fb64681640cd
cewatlac <128319370+cewatlac@users.noreply.github.com>
2025-03-08 14:42:10 +0200

    Create G.cpp

---
+++ Additional Sheet (Condition)/G.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a,c;
+    char b;
+    cin>>a>>b>>c;
+   
+
+     if(b=='+'){
+         cout<<a+c<<endl;
+    }
+   
+    else {
+        cout<<a-c<<endl;
+    }
+    
+  
+    
+    return 0;
+}
