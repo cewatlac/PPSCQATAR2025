fa600effecb939492390fe6bd962f49840a6014a
cewatlac <128319370+cewatlac@users.noreply.github.com>
2025-03-08 15:28:57 +0200

    Create W.cpp

---
+++ Sheet 3 (Loops 1)/W.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while (t--){
+        long long n;
+        cin >> n;
+        cout << (long long)ceil((float)n/2.0) <<"\n" ; 
+    }
+    return 0;
+}
