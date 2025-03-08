7535b0988128c6a111b4c98cd7d138faf0ca593a
cewatlac <128319370+cewatlac@users.noreply.github.com>
2025-03-08 15:08:52 +0200

    Create K.cpp

---
+++ Sheet 3 (Loops 1)/K.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long n ,c= 0;
+    cin >> n;
+    while (n!=0){
+        if ( n % 10 == 4 || n % 10 == 7)c++;
+        n/=10;
+    }
+    if (c == 4 || c == 7)cout << "YES\n";
+    else cout << "NO\n";
+    return 0;
+}
