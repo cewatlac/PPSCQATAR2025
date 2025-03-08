dda62acceba2378b0555329cf2b6a564d5f71431
cewatlac <128319370+cewatlac@users.noreply.github.com>
2025-03-08 13:27:43 +0200

    Create K.cpp

---
+++ Sheet 1 (Arithmetic Operations)/K.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int x , a , b;
+    cin >> x >> a >> b;
+     x =  x - a ;
+     x = x % b ;
+     cout << x ;
+
+return 0;
+}
