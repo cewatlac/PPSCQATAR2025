b35f98a10da01fd4b9880f6d76bc1cab9c35e4b1
cewatlac <128319370+cewatlac@users.noreply.github.com>
2025-03-08 13:31:48 +0200

    Create N.cpp

---
+++ Sheet 1 (Arithmetic Operations)/N.cpp
@@ -0,0 +1,12 @@
+    #include <iostream>
+    #include <algorithm>
+    using namespace std;
+    int main()
+    {
+       int a , b;
+       cin >> a >> b;
+       a *= 3;
+       int pie = (a+b)/2;
+       cout << pie;
+    return 0;
+    }
