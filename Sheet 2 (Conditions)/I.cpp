9012a59773e9f3845d1f37d4f3485aed850ebe64
cewatlac <128319370+cewatlac@users.noreply.github.com>
2025-03-08 14:04:13 +0200

    Create I.cpp

---
+++ Sheet 2 (Conditions)/I.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a , b;
+    cin >> a >> b;
+    if(a+b==3){
+        cout << 3;
+    }
+    else if (a+b == 4){
+        cout << 2;
+    }
+    else cout << 1;
+            return 0;
+    }
+
