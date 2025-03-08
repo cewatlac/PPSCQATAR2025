1e751e1b9579fde133eb787da508dcf405cd7171
cewatlac <128319370+cewatlac@users.noreply.github.com>
2025-03-08 14:49:43 +0200

    Create K.cpp

---
+++ Additional Sheet (Condition)/K.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a;
+   
+    cin>>a;
+   
+
+     if(a>=0){
+         cout<<a<<endl;
+    }
+   
+    else  {
+        cout<<"0"<<endl;
+    }
+    
+  
+    
+    return 0;
+}
