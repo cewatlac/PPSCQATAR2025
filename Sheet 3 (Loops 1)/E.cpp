9969fba080a1f0dccd61a88bf1835fd7a0dc00f4
cewatlac <128319370+cewatlac@users.noreply.github.com>
2025-03-08 15:03:36 +0200

    Create E.cpp

---
+++ Sheet 3 (Loops 1)/E.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--){
+        long long k;
+        cin >> k;
+        int b = ceil(k/5.0) ;
+        cout << b<<"\n";
+    }
+
+return 0;
+}
