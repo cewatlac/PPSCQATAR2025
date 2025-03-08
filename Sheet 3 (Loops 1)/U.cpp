298807175783f64e6e884441b468d97d28693e39
cewatlac <128319370+cewatlac@users.noreply.github.com>
2025-03-08 15:26:59 +0200

    Create U.cpp

---
+++ Sheet 3 (Loops 1)/U.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--){
+        int  n , m  , morb ;
+        cin >> n >> m ;
+        morb = n*m;
+        cout << (morb+1)/2 << "\n";
+        
+    }
+    return 0;
+}
