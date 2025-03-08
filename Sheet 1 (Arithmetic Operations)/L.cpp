34717a82320dbdcb9e486a2b173ef7d1f83265f3
cewatlac <128319370+cewatlac@users.noreply.github.com>
2025-03-08 13:29:21 +0200

    Create L.cpp

---
+++ Sheet 1 (Arithmetic Operations)/L.cpp
@@ -0,0 +1,12 @@
+    #include <iostream>
+    #include <algorithm>
+    using namespace std;
+    int main()
+    {
+        float R , G ;
+        cin >> R >> G;
+        cout << G * 2 - R ;
+
+
+    return 0;
+    }
