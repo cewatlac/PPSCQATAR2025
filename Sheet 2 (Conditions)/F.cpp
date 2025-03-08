fe7aa1681e2c45d321dc70f5dfffbeb0f8a7cb60
cewatlac <128319370+cewatlac@users.noreply.github.com>
2025-03-08 14:02:03 +0200

    Create F.cpp

---
+++ Sheet 2 (Conditions)/F.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char s , t;
+    cin >> s >> t;
+    if (s == 'Y'){
+        t = toupper(t);
+    }
+    cout << t;
+            return 0;
+    }
+
