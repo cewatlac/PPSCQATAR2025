0441b01fe8304e9419d6799ec03e04318c8e56d8
cewatlac <128319370+cewatlac@users.noreply.github.com>
2025-03-08 13:48:44 +0200

    Create T.cpp

---
+++ Sheet 1 (Arithmetic Operations)/T.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a , b;
+    cin >> a >> b;
+    int sm = a+b;
+    cout << sm/2 << " " << a - (sm/2) ; 
+
+return 0;
+}
