aa33fbf1b7ebb5d5918a8d26ea8be3f6ccab0fbf
cewatlac <128319370+cewatlac@users.noreply.github.com>
2025-03-08 16:03:04 +0200

    Create E.cpp

---
+++ Sheet 6 (Arrays 1)/E.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "algorithm"
+using namespace std;
+int main() {
+    int n ,arr[105];
+    cin >> n;
+    for (int  i = 0; i < n ; i ++){
+        cin >> arr[i];
+    }
+    sort(arr , arr+n);
+    for (int i =0 ; i < n ; i ++)cout << arr[i] <<" ";
+    return 0;
+
+}
