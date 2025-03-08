aa9918b54d03faa3a82ead7a4d2e88a4f8a735f6
cewatlac <128319370+cewatlac@users.noreply.github.com>
2025-03-08 13:30:17 +0200

    Create M.cpp

---
+++ Sheet 1 (Arithmetic Operations)/M.cpp
@@ -0,0 +1,11 @@
+    #include <iostream>
+    #include <algorithm>
+    using namespace std;
+    int main()
+    {
+        int a ;
+        cin >> a;
+        a+=1;
+        cout << a / 2;
+    return 0;
+    }
