231707b29f44d11394bb543e2cb88eeee29cf4d9
cewatlac <128319370+cewatlac@users.noreply.github.com>
2025-03-08 14:13:14 +0200

    Create O.cpp

---
+++ Sheet 2 (Conditions)/O.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int k ;
+   cin >> k ;
+   if (k == 1)cout << 1 ;
+   else if (k == 2)cout << 1 ;
+   else if (k == 3)cout << 1 ;
+   else if (k == 4)cout << 2 ;
+   else if (k == 5)cout << 1 ;
+   else if(k == 6)cout << 2 ;
+   else if (k == 7)cout << 1 ;
+   else if (k == 8)cout << 5 ;
+   else if (k == 9)cout << 2 ;
+   else if (k == 10)cout << 2 ;
+   else if (k == 11)cout << 1 ;
+   else if (k == 12)cout << 5 ;
+   else if (k == 13)cout << 1 ;
+   else if (k == 14)cout << 2 ;
+   else if (k == 15)cout << 1 ;
+   else if (k == 16)cout << 14 ;
+   else if (k == 17)cout << 1 ;
+   else if ( k == 18)cout << 5 ;
+   else if (k == 19)cout << 1 ;
+   else if (k == 20)cout << 5 ;
+   else if (k == 21)cout << 2 ;
+   else if (k == 22)cout << 2 ;
+   else if (k == 23)cout << 1 ;
+   else if (k == 24)cout << 15 ;
+   else if (k == 25)cout << 2 ;
+   else if (k == 26)cout << 2 ;
+   else if (k == 27)cout << 5 ;
+   else if (k == 28)cout << 4 ;
+   else if (k == 29)cout << 1 ;
+   else if (k == 30)cout << 4 ;
+   else if (k == 31)cout << 1 ;
+   else cout << 51 ;
+    return 0;
+}
