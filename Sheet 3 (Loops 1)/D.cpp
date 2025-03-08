e776c1e7c18484dbe95fc82469c64a3d841f297e
cewatlac <128319370+cewatlac@users.noreply.github.com>
2025-03-08 15:02:29 +0200

    Update D.cpp

--- Sheet 3 (Loops 1)/D.cpp
+++ Sheet 3 (Loops 1)/D.cpp
@@ -3,15 +3,13 @@ using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(0) ;
-    cin.tie(0) ;
-    int n , ahmed = 0 ;
-    cin >> n;
-    for (int i =0 ; i < n ; i ++){
-        int a , b , c;
-        cin >> a>> b >> c;
-        if (a+b+c >=2)ahmed++;
+    int n , x,  y ,z , sol = 0 ;
+    cin >> n ;
+    for (int i = 0 ;i < n ; i++){
+        cin >> x >> y >> z;
+        if (x+y+z >= 2)sol++;
     }
-    cout << ahmed;
-    return 0;
+    cout << sol;
+    
+return 0;
 }
